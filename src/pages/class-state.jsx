d64edf9b72a206b400716ca3e24a6b63ab644285
freddydc <freddydiaz.liv@gmail.com>
2022-07-07 13:01:15 -0500

    Manage life cycle

--- src/pages/class-state.jsx
+++ src/pages/class-state.jsx
@@ -1,12 +1,39 @@
 import styles from '../styles/Card.module.css'
 import { Component } from 'react'
 
+class Message extends Component {
+  componentWillUnmount() {
+    console.log('COMPONENT WILL UNMOUNT')
+  }
+
+  render() {
+    return <h2>Loading...</h2>
+  }
+}
+
 export class ClassState extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      error: true
+      error: false,
+      loading: false
+    }
+  }
+
+  componentDidMount() {
+    console.log('COMPONENT DID MOUNT')
+  }
+
+  componentDidUpdate() {
+    if (this.state.loading) {
+      setTimeout(() => {
+        try {
+          this.setState({ loading: false })
+        } catch (err) {
+          this.setState({ error: true })
+        }
+      }, 1000)
     }
   }
 
@@ -17,8 +44,11 @@ export class ClassState extends Component {
           <h1>Delete {this.props.name}</h1>
         </div>
         <div>
+          {this.state.loading ? <Message /> : null}
           {this.state.error ? <h2>Error the code is incorrect</h2> : null}
-          {!this.state.error ? <h2>Type the security code</h2> : null}
+          {!this.state.error && !this.state.loading ? (
+            <h2>Type the security code</h2>
+          ) : null}
         </div>
         <div>
           <input placeholder="Cook..." />
@@ -26,7 +56,9 @@ export class ClassState extends Component {
         <div>
           <button
             onClick={() =>
-              this.setState(previousState => ({ error: !previousState.error }))
+              this.setState(previousState => ({
+                loading: !previousState.loading
+              }))
             }
           >
             Check

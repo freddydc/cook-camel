1fa52cdea8a52f5a9725ca4af803026321667127
freddydc <freddydiaz.liv@gmail.com>
2022-07-09 12:59:57 -0500

    Imperative states with useState()

--- src/pages/use-state.jsx
+++ src/pages/use-state.jsx
@@ -7,7 +7,9 @@ export const UseState = ({ name }) => {
   const [state, setState] = useState({
     inputData: '',
     loading: false,
-    error: null
+    error: null,
+    confirmed: false,
+    deleted: false
   })
 
   useEffect(() => {
@@ -18,7 +20,8 @@ export const UseState = ({ name }) => {
             setState({
               ...state,
               loading: false,
-              error: null
+              error: null,
+              confirmed: true
             })
             return
           }
@@ -40,40 +43,103 @@ export const UseState = ({ name }) => {
         state.error && !state.loading ? styles.error : ''
       }`}
     >
-      <div>
-        <h1>Delete {name}</h1>
-      </div>
-      <div>
-        {state.loading ? <h2>Loading...</h2> : null}
-        {state.error && !state.loading ? <h2>{state.error}</h2> : null}
-        {!state.error && !state.loading ? (
-          <h2>Type the security code</h2>
-        ) : null}
-      </div>
-      <div>
-        <input
-          placeholder="Cook..."
-          value={state.inputData}
-          onChange={e =>
-            setState({
-              ...state,
-              inputData: e.target.value
-            })
-          }
-        />
-      </div>
-      <div>
-        <button
-          onClick={() =>
-            setState(previousValue => ({
-              ...state,
-              loading: !previousValue.loading
-            }))
-          }
-        >
-          Check
-        </button>
-      </div>
+      {!state.confirmed ? (
+        <>
+          <div>
+            <h1>Delete {name}</h1>
+          </div>
+          <div>
+            {state.loading ? <h2>Loading...</h2> : null}
+            {state.error && !state.loading ? <h2>{state.error}</h2> : null}
+            {!state.error && !state.loading ? (
+              <h2>Type the security code</h2>
+            ) : null}
+          </div>
+          <div>
+            <input
+              placeholder="Cook..."
+              value={state.inputData}
+              onChange={e =>
+                setState({
+                  ...state,
+                  inputData: e.target.value
+                })
+              }
+            />
+          </div>
+          <div>
+            <button
+              onClick={() =>
+                setState(previousValue => ({
+                  ...state,
+                  loading: !previousValue.loading
+                }))
+              }
+            >
+              Check
+            </button>
+          </div>
+        </>
+      ) : null}
+
+      {state.confirmed && !state.deleted ? (
+        <>
+          <div>
+            <h1>Delete {name}</h1>
+          </div>
+          <div>
+            <h2>Are you sure?</h2>
+          </div>
+          <div>
+            <button
+              onClick={() =>
+                setState({
+                  ...state,
+                  deleted: true
+                })
+              }
+            >
+              Ok
+            </button>
+            <button
+              onClick={() =>
+                setState({
+                  ...state,
+                  confirmed: false,
+                  inputData: ''
+                })
+              }
+            >
+              Cancel
+            </button>
+          </div>
+        </>
+      ) : null}
+
+      {state.deleted ? (
+        <>
+          <div>
+            <h1>{name} Deleted</h1>
+          </div>
+          <div>
+            <h2>Recover {name}</h2>
+          </div>
+          <div>
+            <button
+              onClick={() =>
+                setState({
+                  ...state,
+                  confirmed: false,
+                  deleted: false,
+                  inputData: ''
+                })
+              }
+            >
+              Ok
+            </button>
+          </div>
+        </>
+      ) : null}
     </div>
   )
 }

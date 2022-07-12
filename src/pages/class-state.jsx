f470ec486a575c011869787c4b38a2ea6b72d809
freddydc <freddydiaz.liv@gmail.com>
2022-07-12 10:18:42 -0500

    feat: implement verifier

--- src/pages/class-state.jsx
+++ src/pages/class-state.jsx
@@ -20,7 +20,9 @@ export class ClassState extends Component {
     this.state = {
       inputData: '',
       error: null,
-      loading: false
+      loading: false,
+      confirmed: false,
+      deleted: false
     }
   }
 
@@ -33,21 +35,57 @@ export class ClassState extends Component {
       setTimeout(() => {
         try {
           if (this.state.inputData === KEY_GUARD) {
-            this.setState({
-              loading: false,
-              error: null
-            })
+            this.handleVerifyKey()
             return
           }
           throw new Error('Error the code is incorrect')
         } catch (err) {
-          this.setState({
-            error: err.message,
-            loading: false
-          })
+          this.handleErrorMessage(err)
         }
       }, 1000)
     }
+  }
+
+  handleVerifyKey = () => {
+    this.setState({
+      loading: false,
+      error: null,
+      confirmed: true
+    })
+  }
+
+  handleGetInput = e => {
+    this.setState({ inputData: e.target.value })
+  }
+
+  handleSendVerifyKey = () => {
+    this.setState({ loading: true })
+  }
+
+  handleErrorMessage = error => {
+    this.setState({
+      error: error.message,
+      loading: false
+    })
+  }
+
+  handleUndoChange = () => {
+    this.setState({
+      confirmed: false,
+      inputData: ''
+    })
+  }
+
+  handleResetChange = () => {
+    this.setState({
+      confirmed: false,
+      deleted: false,
+      inputData: ''
+    })
+  }
+
+  handleDeleteData = () => {
+    this.setState({ deleted: true })
   }
 
   render() {
@@ -57,36 +95,61 @@ export class ClassState extends Component {
           this.state.error && !this.state.loading ? styles.error : ''
         }`}
       >
-        <div>
-          <h1>Delete {this.props.name}</h1>
-        </div>
-        <div>
-          {this.state.loading ? <Message /> : null}
-          {this.state.error && !this.state.loading ? (
-            <h2>{this.state.error}</h2>
-          ) : null}
-          {!this.state.error && !this.state.loading ? (
-            <h2>Type the security code</h2>
-          ) : null}
-        </div>
-        <div>
-          <input
-            placeholder="Cook..."
-            value={this.state.inputData}
-            onChange={e => this.setState({ inputData: e.target.value })}
-          />
-        </div>
-        <div>
-          <button
-            onClick={() =>
-              this.setState(previousState => ({
-                loading: !previousState.loading
-              }))
-            }
-          >
-            Check
-          </button>
-        </div>
+        {!this.state.confirmed ? (
+          <>
+            <div>
+              <h1>Delete {this.props.name}</h1>
+            </div>
+            <div>
+              {this.state.loading ? <Message /> : null}
+              {this.state.error && !this.state.loading ? (
+                <h2>{this.state.error}</h2>
+              ) : null}
+              {!this.state.error && !this.state.loading ? (
+                <h2>Type the security code</h2>
+              ) : null}
+            </div>
+            <div>
+              <input
+                placeholder="Cook..."
+                value={this.state.inputData}
+                onChange={this.handleGetInput}
+              />
+            </div>
+            <div>
+              <button onClick={this.handleSendVerifyKey}>Check</button>
+            </div>
+          </>
+        ) : null}
+
+        {this.state.confirmed && !this.state.deleted ? (
+          <>
+            <div>
+              <h1>Delete {this.props.name}</h1>
+            </div>
+            <div>
+              <h2>Are you sure?</h2>
+            </div>
+            <div>
+              <button onClick={this.handleDeleteData}>Ok</button>
+              <button onClick={this.handleUndoChange}>Cancel</button>
+            </div>
+          </>
+        ) : null}
+
+        {this.state.deleted ? (
+          <>
+            <div>
+              <h1>{this.props.name} Deleted</h1>
+            </div>
+            <div>
+              <h2>Recover {this.props.name}</h2>
+            </div>
+            <div>
+              <button onClick={this.handleResetChange}>Ok</button>
+            </div>
+          </>
+        ) : null}
       </div>
     )
   }
